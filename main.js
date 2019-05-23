b22efb01611e8f620d2996b126efa763343c4c0b
Paolo Sivila <paspau2@hotmail.com>
2019-05-23 18:09:57 -0500

    [ UPDATE ] home - Update data store and first step to home development

---
+++ main.js
@@ -0,0 +1,33 @@
+const vm = new Vue({
+  el: 'main',
+  data: {
+    games: [{
+      id: 'roulette',
+      name: 'Roulette Shots',
+      description: 'A Shot Roulette game',
+      image: 'assets/img/roulette.jpeg',
+      version: '1.0.0'
+    },
+    {
+      id: 'liar',
+      name: 'Liar Liar!',
+      description: 'A Counter of Liar game',
+      image: 'assets/img/liar.jpg',
+      version: '1.0.0'
+    },
+    {
+      id: 'test',
+      name: 'Test name',
+      description: 'Test description',
+      image: 'assets/img/no-img-placeholder.png',
+      version: '1.0.0'
+    }
+    ]
+  },
+  methods: {}
+});
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+  console.log('jquery loaded')
+});
